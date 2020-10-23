d680b61c6fee461c3ee1e2d487761d044aa9d23f
Tom Elliott <tomelliottnz@gmail.com>
2020-10-23 16:16:49 +1300

    Rewriting dem mod with latest decision on logic and ui flow

--- DemestModule.R
+++ DemestModule.R
@@ -5,18 +5,35 @@
 DemestModule <- setRefClass(
     "Demographic Modelling Module",
     contains = "CustomModule",
-    fields = list(
-        GUI = "ANY",
-        raw_data = "ANY",
-        response_var = "ANY",
-        response_type = "ANY",
-        exposure_label = "ANY", exposure_var = "ANY",
-        tab_data = "ANY",
-        exposure = "ANY",
-        used_vars = "ANY",
-        vars_ok_btn = "ANY",
-        response_lhfun = "ANY",
-        response_lhfmla = "ANY"
+    properties(
+        fields = list(
+            GUI = "ANY",
+            varnames = "character", vartypes = "character",
+            model_types = "list",
+            response = "character", response_box = "ANY",
+            model = "character", model_box = "ANY",
+            model_fw = "character", model_fw_box = "ANY",
+            secondaryvar = "character", secondaryvar_box = "ANY",
+            exposure_lbl = "ANY",
+            binomial_type = "character", binomial_type_chk = "ANY",
+            reponse_confirmed = "logical"
+            # response_type = "ANY",
+            # exposure_label = "ANY", exposure_var = "ANY",
+            # tab_data = "ANY",
+            # exposure = "ANY",
+            # used_vars = "ANY",
+            # vars_ok_btn = "ANY",
+            # response_lhfun = "ANY",
+            # response_lhfmla = "ANY"
+        ),
+        prototype = list(
+            response = NA_character_,
+            model = NA_character_,
+            model_fw = NA_character_,
+            secondaryvar = NA_character_,
+            binomial_type = NA_character_,
+            reponse_confirmed = FALSE
+        )
     ),
     methods = list(
         initialize = function(gui, name) {
@@ -26,7 +43,8 @@ DemestModule <- setRefClass(
             )
 
             initFields(
-                raw_data = GUI$getActiveData()
+                varnames = names(get_data()),
+                vartypes = sapply(get_data(), iNZightTools::vartype)
             )
 
             install_dependencies(
@@ -52,6 +70,207 @@ DemestModule <- setRefClass(
                 expand = TRUE)
             ii <- 1L
 
+            response_box <<- gcombobox(
+                varnames[vartypes == "num"],
+                selected = 0L,
+                handler = function(h, ...) response <<- svalue(h$obj)
+            )
+            lbl <- glabel("Response: ")
+            tbl_response[ii, 1L, anchor = c(1, 0), expand = TRUE] <- lbl
+            tbl_response[ii, 2:3, expand = TRUE] <- response_box
+            ii <- ii + 1L
+
+            model_types <<- list(
+                "Births" = "Poisson",
+                "Deaths" = "Poisson",
+                "Migration" = "Poisson",
+                "Income" = "Normal",
+                "School" = c("Normal", "Binomial", "Poisson"),
+                "Other" = c("Normal", "Binomial", "Poisson")
+            )
+            model_box <<- gcombobox(
+                names(model_types),
+                selected = 0L,
+                handler = function(h, ...) {
+                    if (length(svalue(h$obj)))
+                        model <<- svalue(h$obj)
+                    else
+                        model <<- NA_character_
+                }
+            )
+            enabled(model_box) <<- FALSE
+            lbl <- glabel("Type: ")
+            tbl_response[ii, 1L, anchor = c(1, 0), expand = TRUE] <- lbl
+            tbl_response[ii, 2:3, expand = TRUE] <- model_box
+            ii <- ii + 1L
+
+            model_fw_box <<- gcombobox(
+                model_types$Other,
+                selected = 0L,
+                handler = function(h, ...) {
+                    if (length(svalue(h$obj)))
+                        model_fw <<- svalue(h$obj)
+                    else
+                        model_fw <<- NA_character_
+                }
+            )
+            enabled(model_fw_box) <<- FALSE
+            lbl <- glabel("Framework: ")
+            tbl_response[ii, 1L, anchor = c(1, 0), expand = TRUE] <- lbl
+            tbl_response[ii, 2:3, expand = TRUE] <- model_fw_box
+            ii <- ii + 1L
+
+            secondaryvar_box <<- gcombobox(
+                character(),
+                selected = 0L,
+                handler = function(h, ...) {
+
+                }
+            )
+            visible(secondaryvar_box) <<- FALSE
+            exposure_lbl <<- glabel("Exposure: ")
+            visible(exposure_lbl) <<- FALSE
+            binomial_type_chk <<- gradio(
+                c("Total", "Failures"),
+                horizontal = TRUE,
+                handler = function(h, ...) binomial_type <<- svalue(h$obj)
+            )
+            visible(binomial_type_chk) <<- FALSE
+            econt <- gvbox()
+            add(econt, exposure_lbl, anchor = c(1, 0))
+            add(econt, binomial_type_chk, anchor = c(1, 0))
+            tbl_response[ii, 1L, anchor = c(1, 0), expand = TRUE] <- econt
+            tbl_response[ii, 2:3, expand = TRUE] <- secondaryvar_box
+            ii <- ii + 1L
+
+
+            ### -------------------------------------- Variable info
+            g_vars <- gexpandgroup("Variable information",
+                container = mainGrp
+            )
+            visible(g_vars) <- FALSE
+            font(g_vars) <- list(weight = "bold")
+
+            g_vars$set_borderwidth(5)
+            tbl_vars <- glayout(container = g_vars,
+                expand = TRUE)
+            ii <- 1L
+
+
+            ### -------------------------------------- Model specification
+            g_likelihood <- gexpandgroup("Model specification",
+                container = mainGrp
+            )
+            visible(g_likelihood) <- FALSE
+            font(g_likelihood) <- list(weight = "bold")
+            g_likelihood$set_borderwidth(5)
+
+            tbl_likelihood <- glayout(container = g_likelihood,
+                expand = TRUE)
+            ii <- 1L
+
+
+            ## --------- initialize signals for when things change:
+            addResponseObserver(function() {
+                # trigger setting response model (based on type)
+                print(glue::glue("Response variable: {response}"))
+                type <- vartypes[varnames == response]
+                print(glue::glue(" * type: {type}"))
+
+                model_names <- model_box$get_items()
+                model_names <- tolower(gsub("s$", "", model_names)) # remove plural
+
+                rt_match <- sapply(model_names, grepl, x = tolower(response))
+                if (any(rt_match)) {
+                    model_box$set_index(which(rt_match))
+                } else {
+                    model_box$set_index(0L)
+                }
+
+                enabled(model_box) <<- !is.na(response)
+
+                # update list of available variables in 'secondary outcome var'
+                # (i.e., exposure/totals)
+
+
+                # update list of available variables in "Variable List"
+
+            })
+            addModelObserver(function() {
+                if (is.na(model)) {
+                    # disable things
+                    enabled(model_fw_box) <<- FALSE
+                    model_fw_box$set_index(0L)
+                    model_fw_box$set_items(model_types$Other)
+                } else if (model == "Other") {
+                    print(glue::glue(" * model: other"))
+                    enabled(model_fw_box) <<- TRUE
+                } else {
+                    print(glue::glue(" * model: {model}"))
+                    fw_opts <- model_types[[model]]
+
+                    blockHandlers(model_fw_box)
+                    model_fw_box$set_index(0L)
+                    model_fw_box$set_items(fw_opts)
+                    unblockHandlers(model_fw_box)
+
+                    if (length(fw_opts) == 1L)
+                        model_fw_box$set_index(1L)
+                    enabled(model_fw_box) <<- length(fw_opts) > 1L
+                }
+
+            })
+            addModelFWObserver(function() {
+                if (is.na(model_fw) || model_fw == "Normal") {
+                    print(glue::glue(" * framework: unknown"))
+                    # disable secondary var stuff:
+                    visible(secondaryvar_box) <<- FALSE
+                    visible(exposure_lbl) <<- FALSE
+                    visible(binomial_type_chk) <<- FALSE
+
+                    return()
+                }
+                print(glue::glue(" * framework: {model_fw}"))
+
+                numvars <- varnames[vartypes == "num" & varnames != response]
+                w <- sapply(numvars, function(v) all(get_data()[[v]] == round(get_data()[[v]])))
+                intvars <- numvars[w]
+
+                secondaryvar_box$set_items(c("", intvars))
+                secondaryvar_box$set_index(1L)
+
+                visible(secondaryvar_box) <<- TRUE
+                visible(exposure_lbl) <<- model_fw == "Poisson"
+                visible(binomial_type_chk) <<- model_fw == "Binomial"
+
+            })
+        },
+        updatePlot = function() {
+            cat("+------+\n")
+            cat("| plot |\n")
+            cat("+------+\n")
+        },
+        close = function() {
+            # any module-specific clean up?
+            # delete temp files, etc ...
+
+            callSuper()
+        },
+        addResponseObserver = function(FUN, ...) {
+            .self$responseChanged$connect(FUN, ...)
+        },
+        addModelObserver = function(FUN, ...) {
+            .self$modelChanged$connect(FUN, ...)
+        },
+        addModelFWObserver = function(FUN, ...) {
+            .self$model_fwChanged$connect(FUN, ...)
+        }
+    )
+)
+
+invisible(list(
+    x = list(
+        y = function() {
             varnames <- colnames(raw_data)
             vartypes <- sapply(raw_data, iNZightTools::vartype)
             response_var <<- gcombobox(varnames[vartypes == "num"],
@@ -177,7 +396,7 @@ DemestModule <- setRefClass(
             tbl_likelihood[ii, 1L, expand = TRUE, anchor = c(1, 0)] <- glabel("Formula :")
             tbl_likelihood[ii, 2:3, expand = TRUE] <- response_lhfmla
 
-
+            updatePlot()
         },
         set_vars = function() {
             if (response_var$get_index() == 0) return()
@@ -231,7 +450,10 @@ DemestModule <- setRefClass(
         },
         ## add new methods to simplify your code
         updatePlot = function() {
-            if (is.null(tab_data)) return()
+            if (is.null(tab_data)) {
+                text(-1, 1, "Select a response variable", adj = 0)
+                return()
+            }
 
             # This function is linked to the "refresh plot" button.
             # It should generate a plot purely from the values
@@ -329,11 +551,12 @@ DemestModule <- setRefClass(
 
         },
         close = function() {
-            cat("Closing module\n")
+            # any module-specific clean up?
+            # delete temp files, etc ...
 
             callSuper()
         }
-    ),
+    )
     ## This is currently required to get around namespace locking
-    where = e
-)
+    # where = e
+))
