fed58853f2edc1c23ea95d79c6201329cbaf372d
Tom Elliott <tom.elliott@auckland.ac.nz>
2022-04-01 14:51:06 +1300

    module running

--- run_module.R
+++ run_module.R
@@ -5,3 +5,13 @@ wd <- file.path(getwd(), "modules")
 # devtools::load_all("../iNZight")
 try(ui$close(), TRUE)
 ui <- iNZight(addonDir = wd)
+
+
+# test installing modules
+mod_dir <- file.path(tempdir(), "modules")
+dir.create(mod_dir)
+
+# devtools::load_all("../iNZight")
+try(ui$close(), TRUE)
+ui <- iNZight(addonDir = mod_dir)
+m <- NewModuleManager$new(ui)
