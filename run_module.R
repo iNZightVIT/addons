d19e787a05a2e3b699a777ef1ba3c2c0180a3624
Tom Elliott <tomelliottnz@gmail.com>
2022-04-13 13:43:04 +1200

    adjust run commands

--- run_module.R
+++ run_module.R
@@ -1,6 +1,16 @@
 # this script allows developers to test their module easily
 wd <- file.path(getwd(), "modules")
 
+# manually install deps
+for (mod in list.dirs(wd, recursive = FALSE)) {
+    f <- file.path(mod, "DESCRIPTION")
+    deps <- desc::desc_get_deps(f)$package
+    gh_deps <- desc::desc_get_field("Github", file = f)
+    remotes::install_github(gh_deps, upgrade = "never")
+    deps <- deps[!deps %in% row.names(installed.packages())]
+    if (length(deps)) install.packages(deps)
+}
+
 # remotes::install_github('iNZightVIT/iNZight')
 # devtools::load_all("../iNZight")
 try(ui$close(), TRUE)
