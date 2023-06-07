5844a57b0168077010e36aa33d35c148458c87c4
Tom Elliott <tomelliottnz@gmail.com>
2023-06-08 10:34:38 +1200

    update modules

--- run_module.R
+++ run_module.R
@@ -2,19 +2,37 @@
 wd <- file.path(getwd(), "modules")
 
 # manually install deps
-for (mod in list.dirs(wd, recursive = FALSE)) {
-    f <- file.path(mod, "DESCRIPTION")
-    deps <- desc::desc_get_deps(f)$package
-    gh_deps <- desc::desc_get_field("Github", file = f)
-    remotes::install_github(gh_deps, upgrade = "never")
-    deps <- deps[!deps %in% row.names(installed.packages())]
-    if (length(deps)) install.packages(deps)
-}
+# for (mod in list.dirs(wd, recursive = FALSE)) {
+#     f <- file.path(mod, "DESCRIPTION")
+#     deps <- desc::desc_get_deps(f)$package
+#     gh_deps <- desc::desc_get_field("Github", file = f)
+#     remotes::install_github(gh_deps, upgrade = "never")
+#     deps <- deps[!deps %in% row.names(installed.packages())]
+#     if (length(deps)) install.packages(deps)
+# }
+
+# tdata <- iNZightTools::smart_read('tourism.csv')
 
 # remotes::install_github('iNZightVIT/iNZight')
+# devtools::load_all("../iNZightTS2")
 # devtools::load_all("../iNZight")
+
+visitorsQ <- iNZightTS2::visitorsQ
+visitorsQ_long <- visitorsQ |>
+    iNZightTools::reshape_data("long", cols = names(visitorsQ)[-1]) |>
+    iNZightTools::rename_vars(c(visitors = "value")) |>
+    dplyr::mutate(
+        gdp = visitors * runif(dplyr::n(), 0.7, 1.2),
+        cat = sample(c("A", "B", "C"), dplyr::n(), replace = TRUE)
+    )
+
 try(ui$close(), TRUE)
-ui <- iNZight(iNZightTS::visitorsQ, addonDir = wd)
+ui <- iNZight(visitorsQ, addonDir = wd)
+ui <- iNZight(visitorsQ_long, addonDir = wd)
+
+#ui <- iNZight(tdata, addonDir = wd)
+
+
 
 
 # test installing modules
